39a97c952c182417ca94e22c73f85c36ef92ddfb
shivanshu06 <102288010+shivanshu06@users.noreply.github.com>
2023-01-28 17:25:54 +0530

    initialcommit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom"
+import Header from "./components/Header";
+import Home from "./components/Home"
+import "./styles/App.scss";
+import "./styles/header.scss"
+import "./styles/Home.scss"
+
+function App(){
+  return(
+    <Router>
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+      </Routes>
+
+    </Router>
+  )
+}
+
+export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Home=()=>{
+    return(
+        <div className="home">
+        
+            <main>
+                <h1>tech</h1>
+                <p>hello world to react</p>
+            </main>
+            
+        </div>
+
+    );
+};
+
+export default Home;
